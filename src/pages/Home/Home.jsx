830603e3ae7c75beb39088d203ee4624b7aee9d3
David Tache <tached@uninorte.edu.co>
2024-04-24 21:22:13 -0500

    updating logo

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,18 @@
+const Home = () => {
+  return (
+    <>
+      <div className="maincontainer">
+        <div className="titlecont">Co-piloto</div>
+        <div className="pagecontainer">
+          <div className="left">most searched</div>
+          <div className="right">
+            <div className="recommendations">recommendations</div>
+            <div className="searchcont">search</div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,22 +1,24 @@
 //import styles
+// import Home from "../Home";
 import styles from "./Login.module.css";
 
-//import react libraries
-// import { useState } from "react";
+//import React libraries
 
 const Login = () => {
-  // const [lechugas, setlechugas] = useState(0);
-
-  // function aumentarlechugas() {
-  //   setlechugas(lechugas + 1);
-  // }
+  function loadhomepage() {
+    return (
+      <>
+        {/* Set routes using browser router to home page and other stuff */}
+      </>
+    );
+  }
 
   return (
     <>
       <div className={styles.maincont}>
         <div className={styles.middle}>
           <div className={styles.left}>
-            <form className={styles.form} action="">
+            <form className={styles.form} method="GET" onSubmit={loadhomepage} >
               <label className={styles.label}>LOGIN</label>
               <input
                 className={styles.inputs}
@@ -35,7 +37,10 @@ const Login = () => {
               <input className={styles.submit} type="submit" value="Sign in" />
             </form>
             <div className={styles.formbottom}>
-              Don&apos;t have an account? <a href="" className={styles.sign}>Sign in!</a>
+              Don&apos;t have an account?{" "}
+              <a href="" className={styles.sign}>
+                Sign in!
+              </a>
             </div>
           </div>
           <div className={styles.right}>
