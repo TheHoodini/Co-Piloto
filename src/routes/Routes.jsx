25f9295bc420f5825bb9575bdb0f89d0cb055268
David Tache <tached@uninorte.edu.co>
2024-05-09 20:23:26 -0500

    Deleting login [No-more]

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -3,17 +3,11 @@ import { createBrowserRouter } from "react-router-dom";
 // routes.js
 import Error from "../pages/Error";
 import Home from "../pages/Home";
-import Login from "../pages/Login";
 
 const routes = createBrowserRouter([
+
   {
     path: "/",
-    name: "login",
-    element: <Login />,
-    errorElement: <Error />,
-  },
-  {
-    path: "/home",
     name: "home",
     element: <Home />,
     errorElement: <Error />,
